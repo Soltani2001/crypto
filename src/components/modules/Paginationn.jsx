1d824a3c251bc0bd824903597c43452a6646afaa
Morteza Soltani <soltani3724@gmail.com>
2024-04-27 09:39:33 -0700

    pagination test

---
+++ src/components/modules/Paginationn.jsx
@@ -0,0 +1,35 @@
+// import "./styles.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Pagination } from "@mui/material";
+export default function App() {
+  const [page, setPage] = useState(1);
+  const [api, setApi] = useState([]);
+  useEffect(() => {
+    handleFetchData();
+  }, [page]);
+  const handleFetchData = async () => {
+    const resData = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos?_page=${page}`
+    );
+    const data = await resData.data;
+    console.log("datas", data);
+    setApi(data);
+  };
+  return (
+    <div className="App">
+      {api.map((val) => (
+        <>
+          <p>{val.title}</p>
+        </>
+      ))}
+
+      <Pagination
+        count={10}
+        color="primary"
+        onChange={(event, value) => setPage(value)}
+        page={page}
+      />
+    </div>
+  );
+}
